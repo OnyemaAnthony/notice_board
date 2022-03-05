cc9703f9d2c03154cc2285c83efad499848cec91
OnyemaAnthony <onyema.anthony@kampanos.world>
2022-03-05 10:24:28 +0000

    change compile sdk

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
 
   @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            stretch: true,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
