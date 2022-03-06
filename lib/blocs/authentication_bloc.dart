6efdd2db56283014cd60ad54c221a02bc2905cdc
OnyemaAnthony <onyema.anthony@kampanos.world>
2022-03-06 11:42:34 +0000

    create auth event

--- lib/blocs/authentication_bloc.dart
+++ lib/blocs/authentication_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:notice_board/models/user_model.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
--- lib/blocs/authentication_state.dart
+++ lib/blocs/authentication_state.dart
@@ -8,3 +8,33 @@ class AuthenticationInitial extends AuthenticationState {
   @override
   List<Object> get props => [];
 }
+class AppStartedEvent extends AuthenticationEvent{
+  @override
+
+  List<Object?> get props => [];
+
+}
+
+class LogInEvent extends AuthenticationEvent{
+  final String email,password;
+
+ const LogInEvent(this.email, this.password);
+
+  @override
+  List<Object?> get props =>[];
+
+}
+class SignUpEvent extends AuthenticationEvent{
+    final UserModel user;
+
+  const SignUpEvent(this.user);
+
+  @override
+  List<Object?> get props =>[];
+
+}
+class LogOutEvent extends AuthenticationEvent{
+  @override
+  List<Object?> get props =>[];
+
+}
