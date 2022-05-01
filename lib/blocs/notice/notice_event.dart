616f9ffa434d71d881ea8fd16ee7692a08736ee7
OnyemaAnthony <onyema.anthony@kampanos.world>
2022-05-01 11:25:52 +0000

    create notice event

--- lib/blocs/notice/notice_event.dart
+++ lib/blocs/notice/notice_event.dart
@@ -3,3 +3,17 @@ part of 'notice_bloc.dart';
 abstract class NoticeEvent extends Equatable {
   const NoticeEvent();
 }
+
+class CreateNoticeEvent extends NoticeEvent{
+
+  @override
+  List<Object?> get props =>[];
+}
+
+class DeleteNoticeEvent extends NoticeEvent{
+  final String noticeI;
+
+const DeleteNoticeEvent(this.noticeI);
+  @override
+  List<Object?> get props =>[];
+}
