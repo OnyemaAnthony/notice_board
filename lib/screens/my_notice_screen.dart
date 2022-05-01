ecdacd49437af0f05f8fc2aa7355804d2fe6e9a5
OnyemaAnthony <onyema.anthony@kampanos.world>
2022-05-01 16:40:50 +0000

    My notice screen WIP

--- lib/screens/my_notice_screen.dart
+++ lib/screens/my_notice_screen.dart
@@ -16,71 +16,117 @@ class MyNoticeScreen extends StatelessWidget {
 
   //thanks for wanting to be a publisher in our platform, your request will be approved by the admin in the next 24 hours
 
-
   @override
   Widget build(BuildContext context) {
-    UserModel user = (BlocProvider
-        .of<AuthenticationBloc>(context)
+    UserModel user = (BlocProvider.of<AuthenticationBloc>(context)
         .state
         .props[0] as UserModel);
     return BlocProvider(
-      create: (context) => NoticeBloc(repository: NoticeRepository())..add(FetchPublishersNoticeEvent(user.id!)),
-      child: Builder(
-          builder: (BuildContext context) {
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('My Notices'),
-                centerTitle: true,
-              ),
-              body: BlocBuilder<NoticeBloc, NoticeState>(
-                builder: (context, state) {
-                  if(state is NoticeLoadingState){
-                    return Utilities.showCircularLoader('Fetching your Notices...');
-                  }else if(state is NoticeLoadedState){
-                    return user.isPublisher!?buildPublishersList(state.noticeDocs):unregisteredPublisher();
-                  }
-                  return Container(
-
-                  );
-                },
-              ),
-            );
-          }
-      ),
+      create: (context) => NoticeBloc(repository: NoticeRepository())
+        ..add(FetchPublishersNoticeEvent(user.id!)),
+      child: Builder(builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('My Notices'),
+            centerTitle: true,
+          ),
+          body: BlocBuilder<NoticeBloc, NoticeState>(
+            builder: (context, state) {
+              if (state is NoticeLoadingState) {
+                return Utilities.showCircularLoader('Fetching your Notices...');
+              } else if (state is NoticeLoadedState) {
+                return user.isPublisher!
+                    ? buildPublishersList(state.noticeDocs)
+                    : unregisteredPublisher(context);
+              }
+              return Container();
+            },
+          ),
+        );
+      }),
     );
   }
-  Widget unregisteredPublisher(){
+
+  Widget unregisteredPublisher(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-    Container(
-    //width: MediaQuery.of(context).size.width,
-    padding: const EdgeInsets.symmetric(vertical: 13),
-    alignment: Alignment.center,
-    decoration: BoxDecoration(
-    borderRadius: const BorderRadius.all(Radius.circular(5)),
-    border: Border.all(color: Colors.white, width: 2),
-    ),
-    child: const Text(
-    'Become a Publisher',
-    style: TextStyle(fontSize: 20, color: Colors.white),
-    ),
-    ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 13),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(5)),
+            border: Border.all(color: Colors.white, width: 2),
+          ),
+          child: GestureDetector(
+            onTap: (){
+              becomePublisher(context);
+            },
+            child: const Text(
+              'Become a Publisher',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ),
       ],
     );
-
   }
-  Widget buildPublishersList(List<DocumentSnapshot> publishersDoc){
-    List<NoticeModel> notices = publishersDoc.map((notice) =>NoticeModel.fromFireStore(notice) ).toList();
-    return publishersDoc.isEmpty? const Center(child: Text('There is no Notice published by you yet '),):
-      ListView.builder(itemCount: notices.length,
-      itemBuilder: (context,index){
-        NoticeModel notice = notices[index];
-        return GestureDetector(
-            onTap: (){
-              Utilities.push(NoticeDetailScreen(notice: notice,),context);
+
+  Widget buildPublishersList(List<DocumentSnapshot> publishersDoc) {
+    List<NoticeModel> notices = publishersDoc
+        .map((notice) => NoticeModel.fromFireStore(notice))
+        .toList();
+    return publishersDoc.isEmpty
+        ? const Center(
+            child: Text('There is no Notice published by you yet '),
+          )
+        : ListView.builder(
+            itemCount: notices.length,
+            itemBuilder: (context, index) {
+              NoticeModel notice = notices[index];
+              return GestureDetector(
+                  onTap: () {
+                    Utilities.push(
+                        NoticeDetailScreen(
+                          notice: notice,
+                        ),
+                        context);
+                  },
+                  child: NoticeList(notice: notice));
             },
-            child: NoticeList(notice: notice));
+          );
+  }
+
+
+  void becomePublisher(BuildContext context) {
+    Widget cancelButton = FlatButton(
+      child: const Text('No'),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: const Text('Yes'),
+      onPressed: () {
+        //noticeBloc.add(DeleteNoticeEvent(widget.notice!.id!));
+        Navigator.pop(context);
+        Navigator.pop(context);
+      },
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: const Text('Delete Notice'),
+      content:
+      const Text('Are you sure you want to become a Publisher?'),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
       },
     );
   }
