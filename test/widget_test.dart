7f5e264095449e20dffebc5602e85f27764879b9
Anthony <50530242+OnyemaAnthony@users.noreply.github.com>
2023-11-08 23:22:03 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
-// tree, read text, and verify that the values of widget properties are correct.
+
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
