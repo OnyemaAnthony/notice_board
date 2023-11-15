32baa2cb3a5c4213643fe916b9a116fd189d1c11
Anthony <50530242+OnyemaAnthony@users.noreply.github.com>
2023-11-15 22:18:25 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
+
 
 
 import 'package:flutter/material.dart';
